0decedff1a7e5c9170c96859373073df6e51534f
Vanita <vanitabarhman9728@gmail.com>
2025-11-13 21:54:06 +0530

    vector in array 13/11/2025

--- vectorArray.cpp
+++ vectorArray.cpp
@@ -15,8 +15,29 @@ cin >>arr[i];
     {
 cout<< arr[i] << "  ";
     };
-    
-    
+}
 
 
+
+
+
+// reverse order
+
+#include <iostream>
+using namespace std;
+int main(){  
+    int arr[] ={10,20,30,40,50};
+    int n =sizeof(arr)/sizeof(n);
+    for(int i=0 ; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout <<endl;
+    int rev[n];
+    for(int i=0 ; i< n; i++){
+        rev[i] = arr[n-1-i];
+    }
+    for(int i =0 ; i< n; i++){
+        cout << rev[i] << " ";
+    }
+    cout <<endl;
 }
